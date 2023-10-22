ae0517c34842e6b4c6059fad1b8dc7c5ee0cabb4
ManavGhaiCode <manavghai12october@gmail.com>
2023-10-22 11:51:43 +0530

    add base

---
+++ src/HungaNut.h
@@ -0,0 +1,7 @@
+#ifndef _HUNGANUT_H_
+#define _HUNGANUT_H_
+
+#include <iostream>
+#include <main.h>
+
+#endif
---
+++ src/main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int main() {
+    return 0;
+}
+
+#endif
