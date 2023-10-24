ad74eed3548d08f2d20d6ccbcbe629934aca49eb
ManavGhaiCode <manavghai12october@gmail.com>
2023-10-24 16:05:49 +0530

    set up everything

---
+++ src/HungaNut.h
@@ -0,0 +1,6 @@
+#ifndef _HUNGANUT_H_
+#define _HUNGANUT_H_
+
+#include <main.h>
+
+#endif
---
+++ src/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <iostream>
+
+int main( void ) {
+    std::cout << "Hunga Engine...\n" << std::endl;
+    return 0;
+}
+
+#endif
