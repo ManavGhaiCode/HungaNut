60a080c8ad1d5a515334ae6266c116dcbf90cba1
ManavGhaiCode <manavghai12october@gmail.com>
2023-10-24 09:06:48 +0530

    fix log.h

--- src/log.h
+++ src/log.h
@@ -6,19 +6,19 @@
 #include <NutLogger/NutLogger.h>
 
 #ifdef DEBUG
-    #define NUT_TRACE (msg) NutLogger::LogTrace (msg)
-    #define NUT_DEBUG (msg) NutLogger::LogDebug (msg)
-    #define NUT_INFO  (msg) NutLogger::LogInfo  (msg)
-    #define NUT_WARN  (msg) NutLogger::LogWarn  (msg)
-    #define NUT_ERROR (msg) NutLogger::LogError (msg)
-    #define NUT_FATAL (msg) NutLogger::LogFatal (msg)
+    #define NUT_TRACE(msg) NutLogger::LogTrace(msg)
+    #define NUT_DEBUG(msg) NutLogger::LogDebug(msg)
+    #define NUT_INFO(msg) NutLogger::LogInfo(msg)
+    #define NUT_WARN(msg) NutLogger::LogWarn(msg)
+    #define NUT_ERROR(msg) NutLogger::LogError(msg)
+    #define NUT_FATAL(msg) NutLogger::LogFatal(msg)
 #elif
-    #define NUT_TRACE (msg) (void*)0
-    #define NUT_DEBUG (msg) (void*)0
-    #define NUT_INFO  (msg) (void*)0
-    #define NUT_WARN  (msg) (void*)0
-    #define NUT_ERROR (msg) (void*)0
-    #define NUT_FATAL (msg) (void*)0
+    #define NUT_TRACE(msg) (void*)0
+    #define NUT_DEBUG(msg) (void*)0
+    #define NUT_INFO(msg) (void*)0
+    #define NUT_WARN(msg) (void*)0
+    #define NUT_ERROR(msg) (void*)0
+    #define NUT_FATAL(msg) (void*)0
 #endif
 
 #endif
