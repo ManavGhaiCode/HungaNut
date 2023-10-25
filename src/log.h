9185a97a22e389aa5707818d0eb120a3148e34a8
ManavGhaiCode <manavghai12october@gmail.com>
2023-10-25 11:15:16 +0530

    add log.h

---
+++ src/log.h
@@ -0,0 +1,23 @@
+#ifndef _LOG_H_
+#define _LOG_H_
+
+#include <Defines.h>
+#include <NutLogger/NutLogger.h>
+
+#ifdef DEBUG
+    #define NUT_TRACE(...) NutLogger::LogTrace(__VA_ARGS__);
+    #define NUT_DEBUG(...) NutLogger::LogDebug(__VA_ARGS__);
+    #define NUT_INFO(...)  NutLogger::LogInfo(__VA_ARGS__);
+    #define NUT_WARN(...)  NutLogger::LogWarn(__VA_ARGS__);
+    #define NUT_ERROR(...) NutLogger::LogError(__VA_ARGS__);
+    #define NUT_TRACE(...) NutLogger::LogFatal(__VA_ARGS__);
+#elif
+    #define NUT_TRACE(...) (void*)0
+    #define NUT_DEBUG(...) (void*)0
+    #define NUT_INFO(...)  (void*)0
+    #define NUT_WARN(...)  (void*)0
+    #define NUT_ERROR(...) (void*)0
+    #define NUT_TRACE(...) (void*)0
+#endif
+
+#endif
