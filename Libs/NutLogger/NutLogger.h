3193285401ce735c6449c84c2c6138bbc205a286
ManavGhaiCode <manavghai12october@gmail.com>
2023-10-25 08:46:48 +0530

    add NutLogger

---
+++ Libs/NutLogger/NutLogger.h
@@ -0,0 +1,50 @@
+#ifndef _NUTLOGGER_H_
+#define _NUTLOGGER_H_
+
+#include <iostream>
+#include "./color.h"
+#include "./format.h"
+
+namespace NutLogger {
+    template<typename... Targs>
+    void LogTrace(char msg[], Targs ...args) {
+        std::cout << WHITE;
+        std::cout << format(msg, args...) << std::endl;
+    }
+
+    template<typename... Targs>
+    void LogDebug(char msg[], Targs ...args) {
+        std::cout << BLUE;
+        std::cout << format(msg, args...) << std::endl;
+    }
+
+    template<typename... Targs> 
+    void LogInfo(char msg[], Targs ...args) {
+        std::cout << GREEN;
+        std::cout << format(msg, args...) << std::endl;
+    }
+
+    template<typename... Targs>
+    void LogWarn(char msg[], Targs ...args) {
+        std::cout << YELLOW;
+        std::cout << format(msg, args...) << std::endl;
+    }
+
+    template<typename... Targs>
+    void LogError(char msg[], Targs ...args) {
+        std::cout << RED;
+        std::cout << format(msg, args...) << std::endl;
+    }
+
+    template<typename... Targs>
+    void LogFatal(char msg[], Targs ...args) {
+        std::cout << BOLDRED;
+        std::cout << format(msg, args...) << std::endl;
+    }
+
+    void Reset() {
+        std::cout << RESET;
+    }
+}
+
+#endif
---
+++ Libs/NutLogger/format.h
@@ -0,0 +1,22 @@
+#ifndef _FORMAT_H_
+#define _FORMAT_H_
+
+#include <iostream>
+
+template<typename T>
+void format_helper(std::string& str, const T& value) {
+    std::size_t openBracket = str.find('{');
+    if (openBracket == std::string::npos) { return; }
+    std::size_t closeBracket = str.find('}', openBracket + 1);
+    if (closeBracket == std::string::npos) { return; }
+    std::cout << str.substr(0, openBracket) << value;
+    str = str.substr(closeBracket + 1);
+}
+
+template<typename... Targs>
+std::string format(std::string str, Targs...args) {
+    (format_helper(str, args),...);
+    return str;
+};
+
+#endif
