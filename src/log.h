815e60127c7ec46021bc455eadff367b2862e462
ManavGhaiCode <manavghai12october@gmail.com>
2023-10-29 14:28:34 +0530

    fix log.h

--- src/log.h
+++ src/log.h
@@ -10,14 +10,14 @@
     #define NUT_INFO(...)  NutLogger::LogInfo(__VA_ARGS__);
     #define NUT_WARN(...)  NutLogger::LogWarn(__VA_ARGS__);
     #define NUT_ERROR(...) NutLogger::LogError(__VA_ARGS__);
-    #define NUT_TRACE(...) NutLogger::LogFatal(__VA_ARGS__);
+    #define NUT_FATAL(...) NutLogger::LogFatal(__VA_ARGS__);
 #elif
     #define NUT_TRACE(...) (void*)0
     #define NUT_DEBUG(...) (void*)0
     #define NUT_INFO(...)  (void*)0
     #define NUT_WARN(...)  (void*)0
     #define NUT_ERROR(...) (void*)0
-    #define NUT_TRACE(...) (void*)0
+    #define NUT_FATAL(...) (void*)0
 #endif
 
 #endif
