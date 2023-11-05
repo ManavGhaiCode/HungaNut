dd1171a1c1473089c8cf0ec2d26374d5cd88cad6
ManavGhaiCode <manavghai12october@gmail.com>
2023-11-05 09:35:09 +0530

    add permake

---
+++ Permake5.lua
@@ -0,0 +1,28 @@
+workspace "HungaNut"
+    architecture "x64"
+
+    -- configs {
+    --     "Debug",
+    --     "Release"
+    -- }
+
+project "HungaNut"
+    kind "StaticLib"
+    language "C++"
+
+    targetdir "Build"
+
+    files {
+        "src/**.h"
+        "src/**.cpp"
+    }
+
+    include {
+        "ThirdParty/SFML/include"
+    }
+
+    -- filter "configs:Debug"
+    --     symbols "On"
+    
+    -- filter "configs:Release"
+    --     oprimize "On"
