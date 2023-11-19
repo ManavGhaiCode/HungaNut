36b667074f548dd87ef77d1bf1cee3f5b7cc7925
ManavGhaiCode <manavghai12october@gmail.com>
2023-11-19 10:01:33 +0530

    reset Physics

--- src/Physics/Physics.h
+++ src/Physics/Physics.h
@@ -1,20 +1,18 @@
 #ifndef _PHYSICS_H_
 #define _PHYSICS_H_
 
-#include <chipmunk.h>
+// namespace Hunga {
+//     class Physics {
+//         public:
+//             void Init();
+//             void ShutDown();
 
-namespace Hunga {
-    class Physics {
-        public:
-            void Init();
-            void ShutDown();
+//             void Update();
 
-            void Update();
-
-        private:
-            cpSpace* m_World;
-            cpVect* m_gravity;
-    };
-}
+//         private:
+//             cpSpace* m_World;
+//             cpVect* m_gravity;
+//     };
+// }
 
 #endif
